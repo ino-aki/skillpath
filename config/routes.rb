19d0a30d5d8bdfd1395029b5c7134be70118b52b
ino-aki <huanghongjingshang043@gmail.com>
2024-07-03 21:13:38 +0900

    ユーザー管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'qualifications#index'
+  resources :qualifications do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
 end
