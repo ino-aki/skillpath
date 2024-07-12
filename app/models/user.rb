c38c8802b8a906325cf760eeb194e82738f8d5ab
ino-aki <huanghongjingshang043@gmail.com>
2024-07-12 20:22:40 +0900

    UserモデルとSnsCredentialモデルを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :nickname, presence: true
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :comments, dependent: :destroy
+  has_many :sns_credentials
 end
